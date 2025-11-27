1401f53db08d8df489393307a51d83061efcf47c
nikhilsairaparla523001-hash <nikhilsairaparla523001@gmail.com>
2025-11-28 01:03:13 +0530

    Update ChatWindow.tsx

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -4,14 +4,23 @@ import { type ChatMessage } from '../types';
 
 interface ChatWindowProps {
     messages: ChatMessage[];
+    isLoading: boolean;
 }
 
-export const ChatWindow: React.FC<ChatWindowProps> = ({ messages }) => {
+export const ChatWindow: React.FC<ChatWindowProps> = ({ messages, isLoading }) => {
     return (
         <div className="space-y-6 px-4">
-            {messages.map((msg, index) => (
-                <Message key={msg.id + '-' + index} message={msg} />
-            ))}
+            {messages.map((msg, index) => {
+                const isLastMessage = index === messages.length - 1;
+                return (
+                     <Message
+                        key={msg.id}
+                        message={msg}
+                        isLoading={isLoading}
+                        isLastMessage={isLastMessage}
+                    />
+                );
+            })}
         </div>
     );
 };
